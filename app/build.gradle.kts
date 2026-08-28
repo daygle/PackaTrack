f646ee05c101eb0c4b01aadea30282e4cf789dd8
daygle <daygle@users.noreply.github.com>
2026-08-29 00:08:56 +1000

    chore: change version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.packatrack.app"
         minSdk = 24
         targetSdk = 37
-        versionCode = 2
-        versionName = "2.0.0"
+        versionCode = 1
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
